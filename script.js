695e4ad3a36f76e2889a22fae6b070078776e7a7
blastcraftmc <scolberg@proton.me>
2023-01-07 16:08:28 -0500

    Remade about page

--- script.js
+++ script.js
@@ -5,3 +5,8 @@ function prevPage() {
 function redirectHome() {
     window.location.replace("home")
 }
+
+function arrow() {
+    document.getElementById('arrow').style.animation="rotate 0.3s linear";
+    window.location.replace(aboutme);
+}
