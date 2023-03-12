4943e931128f4204729e7b0c0c7c0e2479c51ef0
blastcraftmc <scolberg@proton.me>
2023-03-12 13:36:33 -0400

    Fixed About Page

--- script.js
+++ script.js
@@ -15,6 +15,27 @@ function changeThemeLight() {
     document.getElementById("darkElement").classList.toggle('lightMode');
 }
 
+//Auto refresh on window resize
+var w = window,
+d = document,
+e = d.documentElement,
+g = d.getElementsByTagName('body')[0],
+x = w.innerWidth || e.clientWidth || g.clientWidth;
+window.onresize = function(event){
+    var t = w.innerWidth || e.clientWidth || g.clientWidth;
+    if (window.innerWidth <= 479){
+    // load mobile script
+
+        if(window.innerHeight < window.innerWidth){
+        document.location.reload(true);
+        }
+    } else {
+        if (t !== x) {
+            document.location.reload(true);
+        }
+    }
+}
+
 //Image Carousel
 var slidePosition = 1;
 SlideShow(slidePosition);
